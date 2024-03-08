30369bf465ba391e3879ef96a22eb05988a15399
morge-1985 <133426010+morge-1985@users.noreply.github.com>
2024-03-07 21:05:09 -0300

    seg parte

--- busca_endereco/script.js
+++ busca_endereco/script.js
@@ -13,16 +13,38 @@ $(document).ready(function () {
       alert('Digite o cep corretamente');
       return false;
     }
+    loading()
+    setTimeout(function(){
+
+    
     $.ajax({
         url:'https://viacep.com.br/ws/'+cep+'/json/',
         type:'GET',
         dataType:'json',
         success:function(data){
-            console.log(data)
+            console.log(data);
+            $('body').find('#resultados').html('<h2>Resultado da consulta </h2'
+                                      +'<p>Cep: '+data.cep+'</p>'
+                                      +'<p>Logradorou: '+data.logradouro+'</p>'
+                                      +'<p>Bairro: '+data.bairro+'</p>'
+                                      +'<p>Cidade: '+data.localidade+'</p>'
+                                      +'<p>Estado: '+data.uf+'</p>'
+                                      +'<p>Ibge: '+data.ibge+'</p>'
 
+
+                                      );                           
         }
 
         
     })
+  },1000);
+
   }
+  function loading(){
+    $('#resultados').html('<img src="carregando.gif"/>');
+  }
+  $("body").on('click', '#modo-noturno', function () {
+    alert(123);
+    $($('body').toggleClass('dark-mode'))
+  });
 })
